833eeb5de10cff06867e1b150b9ca6a471c29c0d
dingdingteach <129077026+dingdingteach@users.noreply.github.com>
2023-03-29 05:48:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let display = document.getElementById('display');
+const articlesElement = document.getElementById('articles');
 
-function addToDisplay(value) {
-	display.value += value;
-}
+fetch('https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=101&sid2=259')
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const articles = doc.querySelectorAll('.section_list_ranking li');
 
-function clearDisplay() {
-	display.value = '';
-}
+    articles.forEach(article => {
+      const title = article.querySelector('.ranking_headline a').textContent;
+      const date = article.querySelector('.ranking_office').textContent;
 
-function calculate() {
-	try {
-		display.value = eval(display.value);
-	} catch (error) {
-		display.value = 'Error';
-	}
-}
+      const li = document.createElement('li');
+      li.innerHTML = `<span class="title">${title}</span><br><span class="date">${date}</span>`;
+      articlesElement.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
